8c2e31402674a56bdb5ffbe0970c46517576d3d6
AssikShuvo <assikuzzaman@yahoo.com>
2021-03-01 18:59:44 +0600

    M1F1

---
+++ CMS/resources/views/Login/index.blade.php
@@ -0,0 +1,33 @@
+<html>
+    <head>
+        <title>Login</title>
+    </head>
+    <body>
+        <h1>Signin Here</h1>
+
+        <form method="post">
+            <fieldset>
+                <legend>Log In</legend>
+                    <table>
+
+                        <tr>
+                            <td>Email</td>
+                            <td><input type="text" name="email"></td>
+                        </tr>
+
+                        <tr>
+                            <td>Password</td>
+                            <td><input type="password" name="password"></td>
+                        </tr>
+
+                        <tr>
+                            <td></td>
+                            <td><input type="submit" name="submit" value="Login"></td>
+                        </tr>
+
+                    </table>
+            </fieldset>
+        </form>
+
+    </body>
+</html>
---
+++ CMS/routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+
+
+Route::get('/login', [LoginController::class, 'login']);
+
+
+
+
