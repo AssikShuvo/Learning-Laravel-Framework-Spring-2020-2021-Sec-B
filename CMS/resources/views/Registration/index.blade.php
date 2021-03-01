ebafdbd85f7176396890c5ffd475f1f062e609ed
AssikShuvo <assikuzzaman@yahoo.com>
2021-03-01 19:14:32 +0600

    M3F1

---
+++ CMS/resources/views/Registration/index.blade.php
@@ -0,0 +1,73 @@
+<html>
+    <head>
+        <title>Registration</title>
+    </head>
+    <body>
+        <h1>Registration Here</h1>
+
+        <form method="post">
+            <fieldset>
+                <legend>Registration</legend>
+                    <table>
+
+                        <tr>
+                            <td>Full Name</td>
+                            <td><input type="text" name="fullname"></td>
+                        </tr>
+
+                        <tr>
+                            <td>Username</td>
+                            <td><input type="text" name="username"></td>
+                        </tr>
+
+                        <tr>
+                            <td>Email</td>
+                            <td><input type="text" name="email"></td>
+                        </tr>
+
+                        <tr>
+                            <td>Password</td>
+                            <td><input type="text" name="password"></td>
+                        </tr>
+
+                        <tr>
+                            <td>Confirm Password</td>
+                            <td><input type="text" name="cpassword"></td>
+                        </tr>
+
+                        <tr>
+                            <td>Address</td>
+                            <td><input type="text" name="address"></td>
+                        </tr>
+
+                        <tr>
+                            <td>Company Name</td>
+                            <td><input type="text" name="cname"></td>
+                        </tr>
+
+                        <tr>
+                            <td>Phone Number</td>
+                            <td><input type="text" name="pnumber"></td>
+                        </tr>
+
+                        <tr>
+                            <td>City</td>
+                            <td><input type="text" name="city"></td>
+                        </tr>
+
+                        <tr>
+                            <td>Country</td>
+                            <td><input type="text" name="country"></td>
+                        </tr>
+
+                        <tr>
+                            <td></td>
+                            <td><input type="submit" name="submit" value="Sign up"></td>
+                        </tr>
+
+                    </table>
+            </fieldset>
+        </form>
+
+    </body>
+</html>
--- CMS/routes/web.php
+++ CMS/routes/web.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegistrationController;
 
 
 Route::get('/login', [LoginController::class, 'login']);
+
+Route::get('/registration', [RegistrationController::class, 'registration']);
 
 
 
